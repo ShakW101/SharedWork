6424b9eddf52eede9a14e7cce138224fc357412d
ShakW101 <157775686+ShakW101@users.noreply.github.com>
2024-02-16 18:13:13 +0530

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -1,37 +1,57 @@
-package project2;
+
 import java.util.*;
 
 public class Testing {
     public void initializeAndRun(int testCases){
-        Collection<Integer> hashSet = new HashSet<>();
-        Collection<Integer> treeSet = new TreeSet<>();
-        Collection<Integer> linkedHashSet = new LinkedHashSet<>();
-        Collection<Integer> linkedList = new LinkedList<>();
-        Collection<Integer> ArrayList = new ArrayList<>();
-        Collection<Integer> PriorityQueue = new PriorityQueue<>();
-        Collection<Integer> ArrayDeque = new ArrayDeque<>();
+        Set<Integer> hashSet = new HashSet<>();
+        Set<Integer> treeSet = new TreeSet<>();
+        Set<Integer> linkedHashSet = new LinkedHashSet<>();
+        List<Integer> linkedList = new LinkedList<>();
+        List<Integer> ArrayList = new ArrayList<>();
+        Queue<Integer> PriorityQueue = new PriorityQueue<>();
+        Queue<Integer> ArrayDeque = new ArrayDeque<>();
 
-        loadCollection(hashSet);
-        loadCollection(treeSet);
-        loadCollection(linkedHashSet);
-        loadCollection(linkedList);
-        loadCollection(ArrayList);
-        loadCollection(PriorityQueue);
-        loadCollection(ArrayDeque);
+        loadSet(hashSet);
+        loadSet(treeSet);
+        loadSet(linkedHashSet);
+        loadList(linkedList);
+        loadList(ArrayList);
+        loadQueue(PriorityQueue);
+        loadQueue(ArrayDeque);
 
-        testAddSet(hashSet, "Hash Set", testCases);
-        testAddSet(treeSet, "Tree Set", testCases);
-        testAddSet(linkedHashSet, "Linked Hash Set", testCases);
-        testAddSet(linkedList, "Linked List", testCases);
-        testAddSet(ArrayList, "Array List", testCases);
-        testAddSet(PriorityQueue, "Priority Queue", testCases);
-        testAddSet(ArrayDeque, "Array Deque", testCases);
+        testSet(hashSet, "Hash Set","add", testCases);
+        testSet(treeSet, "Tree Set","add", testCases);
+        testSet(linkedHashSet, "Linked Hash Set", "add", testCases);
+        System.out.println();
+        testSet(hashSet, "Hash Set","contains", testCases);
+        testSet(treeSet, "Tree Set","contains", testCases);
+        testSet(linkedHashSet, "Linked Hash Set", "contains", testCases);
+        System.out.println();
+        testSet(hashSet, "Hash Set","remove", testCases);
+        testSet(treeSet, "Tree Set","remove", testCases);
+        testSet(linkedHashSet, "Linked Hash Set", "remove", testCases);
+        System.out.println();
+        testSet(hashSet, "Hash Set","clear", testCases);
+        testSet(treeSet, "Tree Set","clear", testCases);
+        testSet(linkedHashSet, "Linked Hash Set", "clear", testCases);
+//        testAddSet(linkedList, "Linked List", testCases);
+//        testAddSet(ArrayList, "Array List", testCases);
+//        testAddSet(PriorityQueue, "Priority Queue", testCases);
+//        testAddSet(ArrayDeque, "Array Deque", testCases);
 
 
 
     }
 
-    private void loadCollection(Collection<Integer> input){
+    private void loadSet(Set<Integer> input){
+        Random randInt = new Random();
+        for(int i = 0; i < 100000; i++){
+            Integer x = randInt.nextInt(100000);
+            input.add(x);
+        }
+    }
+
+    private void loadList(List<Integer> input){
         Random randInt = new Random();
         for(int i = 0; i < 100000; i++){
             Integer x = Integer.valueOf(randInt.nextInt(100000));
@@ -39,34 +59,73 @@ public class Testing {
         }
     }
 
-    private void testAddSet(Collection<Integer> set, String name, int tests){
+    private void loadQueue(Queue<Integer> input){
+        Random randInt = new Random();
+        for(int i = 0; i < 100000; i++){
+            Integer x = Integer.valueOf(randInt.nextInt(100000));
+            input.add(x);
+        }
+    }
+
+    private void testSet(Set<Integer> input, String name, String test, int testNo){
         long totalTime = 0;
+        long start = 0;
+        long end = 0;
         Random rand = new Random();
-        for(int i = 0; i < tests; i++){
+        for(int i = 0; i < testNo; i++){
             Integer x = Integer.valueOf(rand.nextInt(10000));
-            long start = System.nanoTime();
-            set.add(x);
-            long end = System.nanoTime();
+            if(test.equals("add")) {
+                start = System.nanoTime();
+                input.add(x);
+                end = System.nanoTime();
+                input.remove(i);
+            }else if(test.equals("contains")){
+                start = System.nanoTime();
+                input.contains(x);
+                end = System.nanoTime();
+            }else if(test.equals("remove")){
+                Object[] array =  input.toArray();
+                int index = rand.nextInt(array.length);
+                int element = (int) array[index];
+                start = System.nanoTime();
+                input.remove(element);
+                end = System.nanoTime();
+                input.add(element);
+            }else if(test.equals("clear")){
+                Set<Integer> temp = null;
+
+                if(input instanceof LinkedHashSet<Integer>){
+                    temp = new LinkedHashSet<Integer>(input);
+                } else if(input instanceof HashSet<Integer>){
+                    temp = new HashSet<Integer>(input);
+                } else if(input instanceof TreeSet<Integer>){
+                    temp = new TreeSet<Integer>(input);
+                }
+                    //System.out.println("mmm");
+               // System.out.println(temp);
+                System.out.println(temp.size());
+                start = System.nanoTime();
+
+                temp.clear();
+//                if(input.isEmpty())
+//                    System.out.println("hmm");
+                end = System.nanoTime();
+                //System.out.println(input);
+
+                //System.out.println(end - start);
+
+            }
 
             totalTime += end - start;
-
-            set.remove(i);
         }
 
-        long averageTime = totalTime/tests;
+        long averageTime = totalTime/testNo;
 
-        System.out.printf("%s: %d ns%n", name, averageTime);
+        System.out.printf("%s: %s: %d ns%n", name,test, averageTime);
     }
-//
-//    private void printCollection(List<Integer> arr){
-//        int x = arr.size();
-//        for(int i = 0; i < x; i++){
-//            System.out.println(arr.g);
-//        }
-//    }
     public static void main(String[] args){
         Testing test = new Testing();
-        int testCases = 100;
+        int testCases = 1;
         test.initializeAndRun(testCases);
     }
 }
